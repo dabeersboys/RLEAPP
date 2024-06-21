dbab91d456e4adcf4741b05af57337290af432e4
dabeersboys <68974202+dabeersboys@users.noreply.github.com>
2024-06-21 11:22:16 -0600

    Update discordReturnsunkn.py to support UTC-8 encoding
    Update discordReturnsunkn.py to support UTC-8 encoding for emoji support

--- scripts/artifacts/discordReturnsunkn.py
+++ scripts/artifacts/discordReturnsunkn.py
@@ -16,7 +16,7 @@ def get_discordReturnsunkn(files_found, report_folder, seeker, wrap_text, time_o
         
         if file_found.endswith('.csv'):
             data_list_dm =[]
-            with open(file_found, 'r', errors='backslashreplace') as f:
+            with open(file_found, 'r', encoding='utf-8', errors='backslashreplace') as f:
                 for line in f:
                     delimited = csv.reader(f, delimiter=',')
                     for item in delimited:
